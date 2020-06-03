98c1374cde56bd67346e6bfa4609d80c045090ca
Chandru Kuppuswamy <chandrukuppuswamy@outlook.com>
2020-06-03 13:10:49 +0530

    Adding Order Page

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace BethanysPieShop
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
